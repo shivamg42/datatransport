f75dce25f2d9cc5e741ceb154e5ea7ac16ff1db7
KeerthiGowda <keerthis@vt.edu>
2017-04-13 16:59:14 -0400

    Updated find implementation and added basic filter

--- mongo_xlsx_bridge/export/export_file.js
+++ mongo_xlsx_bridge/export/export_file.js
@@ -1,4 +1,8 @@
 var fs = require('fs');
+var qs = require('mongo-querystring');
+var querystring = require('querystring')
+var q2m = require('query-to-mongo')
+
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var excelbuilder = require('msexcel-builder');
 
@@ -27,7 +31,7 @@ function getDBs(exRule, dbs){
         allCollections = [];
         collections = {};
         properties["property"] = property;
-        properties["filer"] = filter;
+        properties["filter"] = filter;
         properties["columnName"] = columnName;
 
         allProperties.push(properties);
@@ -49,33 +53,51 @@ function getDBs(exRule, dbs){
     }
 }
 
-function createExcel(output){
+function createExcel(output, dbs){
     // Create a new workbook file in current working-path
   var workbook = excelbuilder.createWorkbook('./output/', 'sample.xlsx')
   var columns = output.length;
 
-  var sheet1 = workbook.createSheet('sheet1', 100, 100);
-  console.log(output.length);
-    for(var i=0; i<columns; i++){
-        for(each in output[i]){
-            sheet1.set(i+1, 1, each.toString());
-            for(var j=0 ; j<output[i][each].length; j++){
-                sheet1.set(i+1, j+2, JSON.stringify(output[i][each][j]));
+  var currColumn = 0;
+  // To-do : Fix the number of rows
+  var sheet1 = workbook.createSheet('sheet1', 100, columns);
+
+    // For each database in db
+    for(var eachDB in dbs){
+        // Every collection inside databse
+        for(var eachColl in dbs[eachDB][0]){
+            var columnName = dbs[eachDB][0][eachColl][0]["columnName"];
+            var data;
+            for(var count = 0 ; count<output.length; count++){
+                data = output[count][columnName];
+                if(data != null)
+                    break
+            }
+            ++currColumn;
+            sheet1.set(currColumn, 1, columnName);
+
+            for(var row=0; row<data.length; row++){
+                var string = JSON.stringify(data[row]);
+                string = string.replace(/["\{\}\[\]\']/g, '');
+                var checkString = string.split(":");
+                if(checkString.length == 2){
+                    string = checkString[1];
+                    string = string.replace(/[\']/g,'');
+                }
+                // +2 beacause  row stats with 0. Row 1 has column header
+                sheet1.set(currColumn,row+2, string);
             }
         }
-    }
 
-  // Check this. This is returning not ok
-  workbook.save(function(ok){
-    if (!ok){
-      workbook.cancel();
     }
-    else
-      console.log('congratulations, your workbook created');
+  // Check this. This is returning not ok
+    workbook.save(function(ok){
+        if (!ok){
+          workbook.cancel();
+        }
     });
 
 }
-
 
 
 exports.exportToExcel = function(file){
@@ -99,8 +121,9 @@ exports.exportToExcel = function(file){
             var allDatabaseClosed = function(){
                 openDatabases--;
                 if(openDatabases == 0){
-                    console.log(output);
-                    createExcel(output);
+                    //console.log(output);
+                    createExcel(output, dbs);
+                    return;
                 }
             };
 
@@ -125,75 +148,86 @@ exports.exportToExcel = function(file){
                     allDatabaseClosed();
                 }
             };
+
             //In this connected database, go to each collection.
+            for(var eachCollection in dbs[thisDatabase][0]){
+                var currCollection = eachCollection;
+                db.collection(currCollection, function(err, collection){
+                    if(err){
+                        console.log("Error in collection");
+                        throw err;
+                    }
 
-            for(var i=0; i<dbs[thisDatabase].length ; i++){
-                for(var eachCollection in dbs[thisDatabase][0]){
-                    var currCollection = eachCollection;
-                    db.collection(currCollection, function(err, collection){
-                        if(err){
-                            console.log("Error in collection");
-                            throw err;
+                    var numOfPropInThisCol = dbs[thisDatabase][0][currCollection].length;
+                    for(var eachProperty = 0; eachProperty <  numOfPropInThisCol ; eachProperty++){
+                        var currProperty = dbs[thisDatabase][0][currCollection][eachProperty].property; // 0 - property, 1 -filter
+                        var currFilter = dbs[thisDatabase][0][currCollection][eachProperty].filter;
+                        //console.log(thisDatabase + " " + currCollection +  " " + currProperty + " " + currFilter);
+
+                        var displayItem = {};
+                        displayItem[currProperty] = 1;  // Sets the field to display
+                        displayItem["_id"] = 0;         // Do not display the __ids
+
+                        // Display the whole column if the filter is empty
+                        if(currFilter == "empty"){
+                            currFilter = "";
                         }
 
-                        var numOfPropInThisCol = dbs[thisDatabase][i][currCollection].length;
-                        for(var eachProperty = 0; eachProperty <  numOfPropInThisCol ; eachProperty++){
-                            var currProperty = dbs[thisDatabase][i][currCollection][eachProperty].property; // 0 - property, 1 -filter
-                            //console.log(thisDatabase + " " + currCollection +  " " + currProperty);
-                            var displayItem = {};
-                            displayItem[currProperty] = 1; // Sets the field to display
-                            displayItem["_id"] = 0;
+                        currFilter = currProperty + "" + currFilter;
+                        currFilter = currFilter + "&fields=" + currProperty;
 
-                            // Use the first argument of find fuction for filters.
-                            // Use the second argument to get the list to be displayed.
-                            collection.find({},displayItem).toArray(function(err, items) {
-                                if(err){
-                                    console.log("Error in find");
-                                    throw err;
+                        var query = currFilter;
+                        var q = q2m(querystring.parse(query));
+                        collection.find(q.criteria,displayItem, function(err, resultCursor) {
+                            currFilter = resultCursor.cursorState.cmd.query;
+                            currProperty = resultCursor.cursorState.cmd.fields;
+
+                            currProperty  = Object.keys(currProperty)[0];
+                            var outputObj = {};
+                            var formattedData = [];
+                            var thisColumn = getColumn(dbs, thisDatabase, currCollection, currProperty);
+
+                            // Reference from stackoverflow: https://goo.gl/ROAaxU
+                            resultCursor.nextObject(function fn(err, item) {
+
+                                if (err){
+                                    console.log("error");
+                                    return;
                                 }
-                                // Get the name of this property
-                                var thisProperty = Object.keys(items[0])[0];
-                                // Fix this.
-                                var thisPropertyForCol = Object.keys(items[0])[0];
-                                //console.log(thisDatabase + "/" + collection.s.name + "/" + thisProperty );
-
-                                var currCollection = collection.s.name ;
-                                var thisColumn;
-
-                                // return from call back items shows the fileds with just as attributes.
-                                // Extract the nested document key.
-                                // Need to check for multiple nested objects
-                                if(thisPropertyForCol   == "attributes"){
-                                    var doc = items[0][Object.keys(items[0])[0]];
-
-                                    // Chec if the whole attriutes are queried or a specific field
-                                    if(Object.keys(doc[0]).length == 1)
-                                        thisPropertyForCol  = thisPropertyForCol  + "." + Object.keys(doc[0])[0];
+                                else if(!item) {
+                                    // Done with all objects
+                                    outputObj[thisColumn] = formattedData ;
+                                    output.push(outputObj);
+                                    saveFinished();
+                                    return;
                                 }
-                                //Get the column name corresponding to thisProperty.
-                                for(var line=0 ; line< dbs[thisDatabase][0][currCollection].length ;line++){
-                                    if(thisPropertyForCol  == dbs[thisDatabase][0][currCollection][line].property){
-                                        thisColumn = dbs[thisDatabase][0][currCollection][line].columnName;
-                                        break;
-                                    }
-                                }
-
-                                // Create a JSON like data whose format is
-                                // { columnName : data_correspond_to_it }
-                                var formattedData = [];
-                                var outputObj = {};
-                                for(var i=0; i<items.length ; i++){
-                                     formattedData.push(items[i][thisProperty]);
-                                }
-
-                                outputObj[thisColumn] = formattedData ;
-                                output.push(outputObj);
-                                saveFinished();
+                                setImmediate(fnAction, item, function() {
+                                    resultCursor.nextObject(fn);
+                                });
                             });
-                        }
-                    });
-                }
+
+                            function fnAction(items, callback) {
+                                for(each in items){
+                                     formattedData.push(items[each]);
+                                }
+                                // Check for large data if stackoverflows
+                                return callback();
+                            }
+                        });
+                    }
+                });
             }
         });
+    }
+}
+
+//Get the column name corresponding to thisProperty.
+function getColumn(dbs, thisDatabase, currCollection, currProperty){
+    var thisColumn;
+    for(var line=0 ; line< dbs[thisDatabase][0][currCollection].length ;line++){
+        if(currProperty == dbs[thisDatabase][0][currCollection][line].property){
+            thisColumn = dbs[thisDatabase][0][currCollection][line].columnName;
+            return thisColumn;
+        }
     }
 }
