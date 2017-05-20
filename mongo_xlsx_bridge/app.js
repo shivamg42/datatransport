1a740de5d2007e76d5cd3acd5f3569f65d8e3610
KeerthiGowda <keerthis@vt.edu>
2017-05-20 02:48:46 -0400

    With parser and  filter

--- mongo_xlsx_bridge/app.js
+++ mongo_xlsx_bridge/app.js
@@ -1,38 +1,35 @@
 /* Entry point file*/
 
-/*
-Issues:
-Keerthi:
-1. Works only for one nested database. Exampele,
-    "db1/col1/attributes.ID#/empty". Doesnot work for attributes.ID#.xxx.xxx
-2. Excel colums are not in order of the rule file.
-3. If some thing fails, everything fails. Need to fix the thow of exceptions to
-   handle this.
-4. Need to add filters.
-5. Need to add discriptions and comments to functions.
-
-Hongbo: Please report issues that you see.
-
-*/
 
 var imp = require('./import/import_file');
 var exp = require('./export/export_file');
+var filter = require('./parser/parseText');
 
 /*  Comment / uncomment the following to import to database. Modify the input file name and
     the rule file to add to a partilcular database or collection.
 */
 /****************  Import *************************/
-
+/*
 var ruleToImport = './import/import_rule.json';
 var inputXlsx = './input/input_db3_col3.xlsx'
 var jsonData = imp.readXlsx(inputXlsx);
 imp.updateDatabase(jsonData, ruleToImport);
-
+*/
 /**************** end of Import *****************/
 
 
-
-
+/**
+* Rules for export rule file
+*   -following are leagal rule files
+        1. a>b&c<d,a>=b&c<=d, a!=b|c=d and other varients
+        2. {a>b&c<d}&{e=f|g>h} and other varients of this. The logical operators
+           between the brackets has to be same.
+                    Ex: {}&{}&{}&{}..., {}|{}|{}|{}..
+        3. No spacing has to be provided except for fields that has spaces.
+            Ex: 'attributes.Position Number' has space between Position and Number.
+            So, the following rule is legal
+                attributes.Position Number!=1460000
+*/
 //Uncomment the following to export from database to excel
 /**************** Export ***********************/
 
